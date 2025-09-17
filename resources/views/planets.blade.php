ac2b8cced4431bd4323123bc4cfa3e1bdda41a03
2217221@talnet.nl <wail.ouladbenyoussef@student.rocva.nl>
2025-09-17 10:18:40 +0200

    Added more to planets, including description. It is now viewable as a HTML page

---
+++ resources/views/planets.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Planets</title>
+</head>
+<body>
+
+    <h2>Planets</h2>
+
+    <h3>{{ $intro }}</h5>
+
+    <ul>
+        @foreach ($planets as $planet)
+        <li><strong>{{ $planet['name'] }}</strong><br>{{ $planet['description'] }}</li>
+        @endforeach
+    </ul>
+    
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -16,9 +16,28 @@ Route::get('/greeting', function () {
 // Eigen opdracht Route toegevoegd
 Route::get('/planets', function () {
 
-    $array = ["Uranus", "Jupiter", "Mars", "Aarde", "Saturnus", "Pluto", "Neptunus", "Venus"];
+    $intro = "Here you will see the names and descriptions of four magnificent planets";
 
-    return $array;
+    $planets = [
+        [
+            'name' => 'Mars',
+            'description' => 'Mars is the fourth planet from the Sun and the second-smallest planet in the Solar System, being larger than only Mercury.'
+        ],
+        [
+            'name' => 'Venus',
+            'description' => 'Venus is the second planet from the Sun. It is named after the Roman goddess of love and beauty.'
+        ],
+        [
+            'name' => 'Earth',
+            'description' => 'Our home planet is the third planet from the Sun, and the only place we know of so far thats inhabited by living things.'
+        ],
+        [
+            'name' => 'Jupiter',
+            'description' => 'Jupiter is a gas giant and doesn\'t have a solid surface, but it may have a solid inner core about the size of Earth.'
+        ],
+    ];
+
+    return view('planets', ["intro" => $intro, "planets" => $planets]);
 
 });
 
