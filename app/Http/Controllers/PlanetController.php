72f6f7c922a2b6d254c4689b0d6f9c4cb5fbab49
w-oby <w-oby>
2025-09-29 13:55:29 +0200

    Added Controllers

---
+++ app/Http/Controllers/PlanetController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use illuminate\Support\Collection;
+
+class PlanetController extends Controller
+{
+    
+    private array $planets = [
+        [
+            'name' => 'Mars',
+            'description' => 'Mars is the fourth planet from the Sun and the second-smallest planet in the Solar System, being larger than only Mercury.'
+        ],
+        [
+            'name' => 'Venus',
+            'description' => 'Venus is the second planet from the Sun. It is named after the Roman goddess of love and beauty.'
+        ],
+        [
+            'name' => 'Earth',
+            'description' => 'Our home planet is the third planet from the Sun, and the only place we know of so far thats inhabited by living things.'
+        ],
+        [
+            'name' => 'Jupiter',
+            'description' => 'Jupiter is a gas giant and doesn\'t have a solid surface, but it may have a solid inner core about the size of Earth.'
+        ],
+    ];
+
+    
+    // Enkel de index dus code hoeft niet ingewikkeld
+    public function index() {
+    $intro = "Here you will see the names and descriptions of four magnificent planets";
+    
+    return view('planets', ["intro" => $intro, "planets" => $this->planets]);
+
+    }
+
+    public function show($planet) {
+
+        $normalizedPlanet = ucfirst(strtolower($planet));
+
+        $selectedPlanet = collect($this->planets)->firstWhere('name', $normalizedPlanet);
+
+        return view('planets-select', ['planet' => $selectedPlanet]);
+    
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PlanetController;
 
 // Standaard pagina op localhost:8000 dus geen specifiek url na /
 Route::get('/', function () {
@@ -21,89 +22,89 @@ Route::get('/greeting', function () {
 
 });
 
-// Eigen opdracht Route toegevoegd
-Route::get('/planets', function () {
+// // Eigen opdracht Route toegevoegd
+// Route::get('/planets', function () {
 
-    $intro = "Here you will see the names and descriptions of four magnificent planets";
+//     $intro = "Here you will see the names and descriptions of four magnificent planets";
 
-    $planets = [
-        [
-            'name' => 'Mars',
-            'description' => 'Mars is the fourth planet from the Sun and the second-smallest planet in the Solar System, being larger than only Mercury.'
-        ],
-        [
-            'name' => 'Venus',
-            'description' => 'Venus is the second planet from the Sun. It is named after the Roman goddess of love and beauty.'
-        ],
-        [
-            'name' => 'Earth',
-            'description' => 'Our home planet is the third planet from the Sun, and the only place we know of so far thats inhabited by living things.'
-        ],
-        [
-            'name' => 'Jupiter',
-            'description' => 'Jupiter is a gas giant and doesn\'t have a solid surface, but it may have a solid inner core about the size of Earth.'
-        ],
-    ];
+//     $planets = [
+//         [
+//             'name' => 'Mars',
+//             'description' => 'Mars is the fourth planet from the Sun and the second-smallest planet in the Solar System, being larger than only Mercury.'
+//         ],
+//         [
+//             'name' => 'Venus',
+//             'description' => 'Venus is the second planet from the Sun. It is named after the Roman goddess of love and beauty.'
+//         ],
+//         [
+//             'name' => 'Earth',
+//             'description' => 'Our home planet is the third planet from the Sun, and the only place we know of so far thats inhabited by living things.'
+//         ],
+//         [
+//             'name' => 'Jupiter',
+//             'description' => 'Jupiter is a gas giant and doesn\'t have a solid surface, but it may have a solid inner core about the size of Earth.'
+//         ],
+//     ];
 
-    if (request()->has('planet')) {
+//     if (request()->has('planet')) {
 
-        $chosenPlanet = ucfirst(request('planet'));
+//         $chosenPlanet = ucfirst(request('planet'));
 
-        $filteredPlanets = collect($planets)
-            ->where('name', $chosenPlanet)
-            ->toArray();
+//         $filteredPlanets = collect($planets)
+//             ->where('name', $chosenPlanet)
+//             ->toArray();
 
-    } else {
+//     } else {
 
-        $filteredPlanets = $planets;
+//         $filteredPlanets = $planets;
 
-    }
+//     }
 
-    return view('planets', ["intro" => $intro, "planets" => $filteredPlanets]);
+//     return view('planets', ["intro" => $intro, "planets" => $filteredPlanets]);
 
-});
+// });
 
-// Dynamic Route voor planets
-Route::get('/planets/{planet}', function ($planet) {
+// // Dynamic Route voor planets
+// Route::get('/planets/{planet}', function ($planet) {
 
-    $planets = [
-        [
-            'name' => 'Mars',
-            'description' => 'Mars is the fourth planet from the Sun and the second-smallest planet in the Solar System, being larger than only Mercury.'
-        ],
-        [
-            'name' => 'Venus',
-            'description' => 'Venus is the second planet from the Sun. It is named after the Roman goddess of love and beauty.'
-        ],
-        [
-            'name' => 'Earth',
-            'description' => 'Our home planet is the third planet from the Sun, and the only place we know of so far thats inhabited by living things.'
-        ],
-        [
-            'name' => 'Jupiter',
-            'description' => 'Jupiter is a gas giant and doesn\'t have a solid surface, but it may have a solid inner core about the size of Earth.'
-        ],
-    ];
+//     $planets = [
+//         [
+//             'name' => 'Mars',
+//             'description' => 'Mars is the fourth planet from the Sun and the second-smallest planet in the Solar System, being larger than only Mercury.'
+//         ],
+//         [
+//             'name' => 'Venus',
+//             'description' => 'Venus is the second planet from the Sun. It is named after the Roman goddess of love and beauty.'
+//         ],
+//         [
+//             'name' => 'Earth',
+//             'description' => 'Our home planet is the third planet from the Sun, and the only place we know of so far thats inhabited by living things.'
+//         ],
+//         [
+//             'name' => 'Jupiter',
+//             'description' => 'Jupiter is a gas giant and doesn\'t have a solid surface, but it may have a solid inner core about the size of Earth.'
+//         ],
+//     ];
 
-    // Zo blijven de bestaande urls nog werken, ook al heb je bijvoorbeeld "juPiTer" ingevoerd.
-    function stringCheck($input) {
+//     // Zo blijven de bestaande urls nog werken, ook al heb je bijvoorbeeld "juPiTer" ingevoerd.
+//     function stringCheck($input) {
 
-        $lowerString = strtolower($input);
+//         $lowerString = strtolower($input);
 
-        $uppercasePlanet = ucfirst($lowerString);
+//         $uppercasePlanet = ucfirst($lowerString);
         
-        return $uppercasePlanet;
+//         return $uppercasePlanet;
 
-    }
+//     }
 
-    $normalizedPlanet = stringCheck($planet);
+//     $normalizedPlanet = stringCheck($planet);
 
-    // Collections en filteren op eerste result dat geselecteerde planeet naam heeft
-    $selectedPlanet = collect($planets)->firstWhere('name', $normalizedPlanet);
+//     // Collections en filteren op eerste result dat geselecteerde planeet naam heeft
+//     $selectedPlanet = collect($planets)->firstWhere('name', $normalizedPlanet);
 
-    return view('planets-select', ['planet' => $selectedPlanet]);
+//     return view('planets-select', ['planet' => $selectedPlanet]);
 
-});
+// });
 
 
 // Experimenteel Route toegevoegd
@@ -122,3 +123,9 @@ Route::get('/colors', function () {
     return view('colors.index', ["greeting" => $welcome, "colorList" => $array]);
 
 });
+
+// PlanetController Route
+Route::get('/planets', [PlanetController::class, 'index']);
+
+// PlanetController Route
+Route::get('/planets/{planet}', [PlanetController::class, 'show']);
